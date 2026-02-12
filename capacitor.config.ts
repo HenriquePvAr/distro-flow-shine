a9db50209141e99357bfc33edd275cfbcf8c2dc3
Henrique Paiva <henriquepaiva128@gmail.com>
2026-02-12 18:44:17 -0300

    agr vai55

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.henrique.distroflow',
-  appName: '2G',
-  webDir: 'dist'
+  appId: "com.henrique.distroflow",
+  appName: "2G",
+  webDir: "dist",
+  bundledWebRuntime: false,
+
+  server: {
+    url: "https://distro-flow-shine.vercel.app",
+    cleartext: false,
+    androidScheme: "https",
+  },
+
+  android: {
+    allowMixedContent: false,
+  },
 };
 
 export default config;
